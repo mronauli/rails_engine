6b4807c55471b5087a1416757741af41b4ef779a
Maria Ronauli <mronauli@gmail.com>
2020-03-21 12:18:44 -0600

    add namespaced routes for items resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get "/find", to: "search#show"
+      end
+      resources :items, except: [:new, :edit]
       resources :merchants, except: [:new, :edit]
     end
   end
