684ed8bba5f6c24fc5279f43a6d0ea6968e52c7f
Maria Ronauli <mronauli@gmail.com>
2020-03-23 03:39:56 -0600

    remove serializer from update action in merchants controller

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
+  end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+  end
+
+  def update
+    render json: Merchant.update(params[:id], merchant_params)
+  end
+
+  def destroy
+    Merchant.delete(params[:id])
+  end
+
+  private
+    def merchant_params
+      params.permit(:name)
+    end
+end
