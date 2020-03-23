593e409d4eb33b83eb7802802789198c2c8e9a86
Maria Ronauli <mronauli@gmail.com>
2020-03-23 03:28:16 -0600

    remove unit_price conversion from rake task to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,22 @@
 class Item < ApplicationRecord
+  extend SearchAttributes
+  validates_presence_of :name, :description
+  validates_presence_of :unit_price, numericality: { greater_than: 0 }
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-  validates_presence_of :name, :description
-  validates_presence_of :unit_price, numericality: { greater_than: 0 }
+  has_many :transactions, through: :invoices
+
+  before_create do
+    self.unit_price = self.unit_price / 100.0 unless self.count_decimal == 2
+  end
+
+  def self.search(params)
+    where('name ILIKE :params' || 'description ILIKE :params', params: "%#{params[:description]}%", params: "%#{params[:name]}%" || :created_at == params[:created_at]
+      .to_date || :updated_at == params[:updated_at].to_date || :unit_price == params[:created_at].to_f)
+  end
+
+  def count_decimal
+    self.unit_price.to_s.split('.').last.size
+  end
 end
